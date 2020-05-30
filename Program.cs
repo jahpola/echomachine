62af47fc3b961a8c818191d6e49280116d9ddd25
Jyrki Ahpola <jyrkia@gmail.com>
2020-05-30 23:25:57 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EchoMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is EchoMachine");
+        }
+    }
+}
